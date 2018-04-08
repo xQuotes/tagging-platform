20b1707770bdd2a9ea50b316ed945dd94eef646a
zff <zhengfangfang@chinadas.com>
2018-04-08 15:58:12 +0800

    第一次提交：项目模板搭建

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-#tagging-platform
+# tagging-platformplatform
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:3001
+npm run dev
+
+# build for production with minification
+npm run build
+
+```
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import $ from 'jquery'
+import App from './App'
+import router from './router'
+import VueRouter from 'vue-router'
+import axios from './http'
+Vue.prototype.axios = axios;
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
