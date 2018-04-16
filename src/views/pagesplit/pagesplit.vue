3c39d0d5c8951648d1f9e3184c143b1a0362ece4
root <13511068453@163.com>
2018-04-16 10:31:52 +0800

    首页切换

---
+++ src/views/pagesplit/pagesplit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pagesplit" v-title="'页切分标注'">
+    页切分标注
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/textsplit/textsplit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="textsplit" v-title="'文本识别标注'">
+     文本识别
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
