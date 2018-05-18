cac4435ade1a9de373da794c3f712212241b6451
付引 <xq@fuyindeMac-mini.lan>
2018-05-18 19:05:36 +0800

    修改导航的点击区域

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,33 +3,33 @@
     <div class="sidebar-wrapper">
             <div class="sidebar">
               <ul class="sidebar-menu">
-                <li :class="{active:linkTo('/home')}">
-                  <router-link to="/home">
-                    首页
-                  </router-link>
-                </li>
-                <li :class="{active:linkTo('/pagesplit')}">
-                  <router-link to="/pagesplit">
-                    页切分标注
-                  </router-link>
-                </li>
+                <router-link to="/home">
+                  <li :class="{active:linkTo('/home')}">
+                      首页
+                  </li>
+                </router-link>
+                <router-link to="/pagesplit">
+                  <li :class="{active:linkTo('/pagesplit')}">
+                      页切分标注
+                  </li>
+                </router-link>
                 <li :class="{active:linkTo('/columnsplit')}">
                   <router-link to="/columnsplit">
                     列切分标注
                   </router-link>
                 </li>
                 <!--<li>字框切分标注</li>-->
-                <li :class="{active:linkTo('/fontsplit')}">
-                  <router-link to="/fontsplit">
-                    字框切分标注
-                  </router-link>
-                </li>
-                <!--<li>文本识别标注</li>-->
-                <li :class="{active:linkTo('/textsplit')}">
-                <router-link to="/textsplit">
-                   文本识别标注
+                <router-link to="/fontsplit">
+                  <li :class="{active:linkTo('/fontsplit')}">
+                      字框切分标注
+                  </li>
                 </router-link>
-                </li>
+                <!--<li>文本识别标注</li>-->
+                <router-link to="/textsplit">
+                  <li :class="{active:linkTo('/textsplit')}">
+                    文本识别标注
+                  </li>
+                </router-link>
               </ul>
             </div>
           </div>
@@ -38,58 +38,58 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      msg: ''
-    }
+      msg: ""
+    };
   },
-  methods:{
-  	 linkTo(path){
-       var route_link = this.$route.path;
-       if(path === route_link){
-         return true
-       }
-     },
+  methods: {
+    linkTo(path) {
+      var route_link = this.$route.path;
+      if (path === route_link) {
+        return true;
+      }
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-	.sidebar-wrapper{
-          .sidebar{
-            width: 150px;
-            float: left;
-            position: relative;
-            margin-right: -100%;
-            .sidebar-menu{
-              border-left:1px solid #ccc;
-              li{
-                padding:0 10px;
-                height:36px;
-                line-height:36px;
-                text-align: center;
-                border:1px solid #ccc;
-                border-left:0 none;
-                margin-bottom:10px;
-                font-size:14px;
-                &.active{
-                  background-color: #D89020;
-                  border-color: #D89020;
-                  a{
-                    color: #fff;
-                  }
-                }
-                &.router-link-active{
-                  background-color: #D89020;
-                  border-color: #D89020;
-                  color: #fff;
-                  a{
-                    color: #fff;
-                  }
-                }
-              }
-            }
+.sidebar-wrapper {
+  .sidebar {
+    width: 150px;
+    float: left;
+    position: relative;
+    margin-right: -100%;
+    .sidebar-menu {
+      border-left: 1px solid #ccc;
+      li {
+        padding: 0 10px;
+        height: 36px;
+        line-height: 36px;
+        text-align: center;
+        border: 1px solid #ccc;
+        border-left: 0 none;
+        margin-bottom: 10px;
+        font-size: 14px;
+        &.active {
+          background-color: #d89020;
+          border-color: #d89020;
+          a {
+            color: #fff;
           }
         }
+        &.router-link-active {
+          background-color: #d89020;
+          border-color: #d89020;
+          color: #fff;
+          a {
+            color: #fff;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
